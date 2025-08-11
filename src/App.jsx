0912eb9585f5eaf727a658442f8892e8d41cf744
ArmanWhy <armanyusuf12@gmail.com>
2025-08-11 13:54:21 +0530

    Add env var console log for debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ChatPage from "./components/ChatPage";
 
+console.log("Backend URL from env:", process.env.REACT_APP_BACKEND_URL);
+
+
 function App() {
   return <ChatPage />;
 }
