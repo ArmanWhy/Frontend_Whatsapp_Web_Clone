4417481d92b67a5cc04c7582b8c50b897b4be133
ArmanWhy <armanyusuf12@gmail.com>
2025-08-11 14:01:54 +0530

    Use environment variable for socket backend URL

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,11 @@
 import { io } from 'socket.io-client';
 
-// Change URL to backend's address
+// Use of env variable with fallback to localhost for local dev
+const backendURL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
 
-const socket = io('http://localhost:5000', {
+console.log("Connecting to Socket.io at:", backendURL);
+
+const socket = io(backendURL, {
     transports: ['websocket', 'polling'],
 });
 
